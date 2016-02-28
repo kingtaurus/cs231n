5c02f133b8b9f0a8413529c3d107fe819d34d0b9
Gregory King <greg.james.king@gmail.com>
2016-02-28 15:30:32 -0800

    Updated data_utils.py for python3 usage

--- old_assignments/assignment1/cs231n/data_utils.py
+++ old_assignments/assignment1/cs231n/data_utils.py
@@ -1,11 +1,11 @@
-import cPickle as pickle
+import pickle
 import numpy as np
 import os
 
 def load_CIFAR_batch(filename):
   """ load single batch of cifar """
   with open(filename, 'rb') as f:
-    datadict = pickle.load(f)
+    datadict = pickle.load(f, encoding='latin1')
     X = datadict['data']
     Y = datadict['labels']
     X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float")
