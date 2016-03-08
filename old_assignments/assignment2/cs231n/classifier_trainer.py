090de4498ada768177fdac095e6c2fd42a4f92c1
Gregory King <greg.james.king@gmail.com>
2016-03-08 10:47:51 -0800

    Added alternative gradient descent algorithms; (1) SGD momentum; (2) RMSProp;

--- old_assignments/assignment2/cs231n/classifier_trainer.py
+++ old_assignments/assignment2/cs231n/classifier_trainer.py
@@ -97,7 +97,8 @@ class ClassifierTrainer(object):
           # step_cache[p] and the momentum strength is stored in momentum.    #
           # Don't forget to also update the step_cache[p].                    #
           #####################################################################
-          pass
+          self.step_cache[p] = momentum * self.step_cache[p] - learning_rate * grads[p]
+          dx = self.step_cache[p]
           #####################################################################
           #                      END OF YOUR CODE                             #
           #####################################################################
@@ -110,7 +111,8 @@ class ClassifierTrainer(object):
           # TODO: implement the RMSProp update and store the parameter update #
           # dx. Don't forget to also update step_cache[p]. Use smoothing 1e-8 #
           #####################################################################
-          pass
+          self.step_cache[p] = decay_rate * self.step_cache[p] + (1 - decay_rate) * grads[p]**2
+          dx = -learning_rate * grads[p] / np.sqrt(self.step_cache[p] + 1e-8)
           #####################################################################
           #                      END OF YOUR CODE                             #
           #####################################################################
