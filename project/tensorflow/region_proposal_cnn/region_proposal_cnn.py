099308751706e8b95a558695ee72d55945b1fa5c
Gregory King <greg.james.king@gmail.com>
2016-07-26 18:04:58 -0700

    Removing superfluous code;

--- project/tensorflow/region_proposal_cnn/region_proposal_cnn.py
+++ project/tensorflow/region_proposal_cnn/region_proposal_cnn.py
@@ -14,10 +14,6 @@ plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-from sklearn.metrics import confusion_matrix
-
-classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
 import tensorflow as tf
 
 
